9c7f06f0e135b8f1840b1a38f5c6b1d0294b46d9
Elias Stihl <eliasstihl@gmail.com>
2024-05-07 16:55:21 +0200

    lstm first version

--- lstm.py
+++ lstm.py
@@ -1,45 +1,30 @@
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 import numpy as np
 import torch.optim as optim
 
 
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
+class CharLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers):
+        super(CharLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.input_size = input_size
+        self.num_layers = num_layers
 
-        self.U = nn.Linear(input_size, hidden_size, dtype=float, bias=True)
-        self.W = nn.Linear(hidden_size, hidden_size, dtype=float, bias=True)
-        self.V = nn.Linear(hidden_size, output_size, dtype=float, bias=True)
-
-        # Initialize weights using normal distribution scaled by 0.01
-        nn.init.normal_(self.U.weight, std=0.01)
-        nn.init.normal_(self.W.weight, std=0.01)
-        nn.init.normal_(self.V.weight, std=0.01)
-
-        # Initialize biases to zero (common practice)
-        nn.init.zeros_(self.U.bias)
-        nn.init.zeros_(self.W.bias)
-        nn.init.zeros_(self.V.bias)
+        self.lstm = nn.LSTM(input_size,hidden_size,num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size,output_size)
+       
+        
 
     def forward(self, input, hidden):
-        batch_size = input.size(0)
-        output = torch.zeros((batch_size, self.input_size), dtype=torch.float)
-
-        for i in range(batch_size):
-            hidden = torch.tanh(self.U(input[i]) + self.W(hidden))
-            output[i, :] = torch.log_softmax(self.V(hidden), dim=1)
-
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size, dtype=float)
+        out, hidden = self.lstm(input, hidden)
+        out = self.linear(out)
+        
+        return out, hidden
+    
+    def init_hidden(self):
+        hidden = (torch.zeros((1, 1, self.hidden_size)), torch.zeros((1, 1, self.hidden_size)))
+        return hidden
 
 
 def load_data(filepath):
@@ -48,14 +33,16 @@ def load_data(filepath):
     return np.array(list(data))
 
 
-def synthesize(rnn, hprev, x0, n):
+def synthesize(model, hprev, x0, n):
     h_t = hprev
     x_t = x0
-    Y = torch.zeros((n, rnn.input_size), dtype=float)
-
+    
+    Y = torch.zeros((n, model.input_size), dtype=float)
+    
     for t in range(n):
-        output, h_t = rnn(x_t, h_t)
+        output, h_t = model(x_t, h_t)
         p_t = output.detach().numpy()
+        p_t = p_t.flatten()
         p_t = np.exp(p_t)
         p_t = p_t / np.sum(p_t)
 
@@ -68,12 +55,11 @@ def synthesize(rnn, hprev, x0, n):
         # Update Y and x_t for next iteration
         Y[t, ii] = 1
         x_t = torch.zeros_like(x0)
-        x_t[0, ii] = 1
+        x_t[0, 0, ii] = 1
 
     return Y
 
-
-def train_rnn():
+def train_model():
     torch.manual_seed(0)
     book_data = load_data('data/goblet_book.txt')
     book_chars = np.unique(book_data)
@@ -89,12 +75,13 @@ def train_rnn():
     eta = 0.001
     gamma = 0.9
     seq_length = 25
-    rnn = RNN(K, m, K)
+    num_layers = 1
+    model = CharLSTM(K, m, K, num_layers)
 
     criterion = nn.CrossEntropyLoss(reduction='sum')
 
     # TODO: Undersök om detta är korrekt.
-    optimizer = optim.RMSprop(rnn.parameters(), lr=eta)
+    optimizer = optim.RMSprop(model.parameters(), lr=eta)
 
     smooth_loss = None
 
@@ -105,39 +92,42 @@ def train_rnn():
     epoch = 1
 
     temp = 0
+    batch_size = 1
+
+    torch.rand
     while epoch <= 3:
         print(f'-------------')
         print(f'Epoch {epoch}')
 
-        rnn.zero_grad()
-        hidden = rnn.initHidden()
+        model.zero_grad()
+        hidden = model.init_hidden()
+        
+        
         for i in range(0, len(book_data) - seq_length, seq_length):
             # Prepare inputs and targets
             X_chars = book_data[i:i+seq_length]
             Y_chars = book_data[i+1:i+seq_length+1]
 
-            X = torch.zeros((seq_length, K), dtype=float)
-            Y = torch.zeros((seq_length, K), dtype=float)
+            X = torch.zeros((1,seq_length, K), dtype=torch.float32)
+            Y = torch.zeros((seq_length, K), dtype=torch.float32)
 
             # One-hot encode inputs and outputs
+            # TODO make compatible with larger batches?
             for t, (x_char, y_char) in enumerate(zip(X_chars, Y_chars)):
-                X[t, char_to_ind[x_char]] = 1
+                X[0,t, char_to_ind[x_char]] = 1
                 Y[t, char_to_ind[y_char]] = 1
 
             # Forward pass
-            output, hidden = rnn(X, hidden)
-
+            output, hidden = model(X, hidden)
+            unbatched_output = torch.reshape(output, (seq_length,K))
+            
             # Backward pass
-            loss = criterion(output, Y)
-
-            # TODO: Undersök detta. Tror konsekvensen är att vi inte backpropagar genom hidden state.
-            hidden = hidden.detach()
-
+            loss = criterion(unbatched_output, Y)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(rnn.parameters(), 0.001)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.001)
             optimizer.step()
-
-            hidden
+            hidden = (hidden[0].detach(), hidden[1].detach())
+    
 
             if smooth_loss is None:
                 smooth_loss = loss
@@ -152,9 +142,11 @@ def train_rnn():
                 print(f'Iter {iteration:7d}. Smooth loss {smooth_loss:7.2f}. Loss {loss:7.2f}.')
 
             if iteration % 10000 == 0:
-                x0 = X[0, :].reshape(1, -1)
-                hprev = rnn.initHidden()
-                Y_synth = synthesize(rnn, hprev, x0, 200)
+               
+                x0 = torch.zeros(1,1,K)
+                x0[0,0,np.random.randint(K)] = 1
+                hprev = model.init_hidden()
+                Y_synth = synthesize(model, hprev, x0, 200)
 
                 txt = ''.join([ind_to_char[torch.argmax(y).item()]
                               for y in Y_synth])
@@ -168,4 +160,4 @@ def train_rnn():
 
 
 if __name__ == '__main__':
-    train_rnn()
+    train_model()
